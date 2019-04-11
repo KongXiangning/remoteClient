0e93083fdcd239e0fbb61aed6722b9e6a1258d25
kxn <kxn@foxmail.com>
2019-04-11 18:05:51 +0800

    new interfaces,添加操作ID

--- wsConn/binaryDecoder.go
+++ wsConn/binaryDecoder.go
@@ -29,7 +29,7 @@ func (conn *Connection) binaryDecoder(data []byte) {
 		fallthrough
 	case 1:
 		ivs := data[2:readIndex]
-		msgData = data[60:]
+		msgData = data[82:]
 		msgData, err = utils.Aes128Decrypt(msgData, utils.SKey, ivs)
 		if err != nil {
 			//conn.WriteMessage()
@@ -39,7 +39,7 @@ func (conn *Connection) binaryDecoder(data []byte) {
 		fallthrough
 	case 3:
 		ivs := data[2:readIndex]
-		msgData = data[60:]
+		msgData = data[82:]
 		msgData, err = utils.Aes128Decrypt(msgData, utils.SKey, ivs)
 		if err != nil {
 			//conn.WriteMessage()
@@ -48,12 +48,12 @@ func (conn *Connection) binaryDecoder(data []byte) {
 		msgData, err = utils.GzipDecode(msgData)
 	}
 
-	method = string(data[readIndex+2 : readIndex+2+data[readIndex+1]])
+	method = string(data[28 : 28+data[27]])
 	fmt.Println(method)
 	fmt.Println(string(msgData))
 	fmt.Println(string(utils.SKey))
 
-	kubeTransfer := execute.KubeTransfer{data[57], method, "", msgData}
+	kubeTransfer := execute.KubeTransfer{Types: data[81], Method: method, Hid: data[19:27], HandleJson: msgData}
 	client := execute.GetClient()
 	client.Execute(kubeTransfer, conn.transferChan)
 }
